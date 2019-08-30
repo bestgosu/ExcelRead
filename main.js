153d3a32039d1ed810b5a5e330b5daaffce7a44d
gosu <bestgo82@gmail.com>
2019-08-30 17:51:01 +0900

    ing

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let path = require("path");
-let excelRead = require("./index.js");
+let { excelRead } = require("./index.js");
 
 excelRead(path.join(".","example.xlsx"),1,2,5).then(datas=>{
 	console.log(datas);
